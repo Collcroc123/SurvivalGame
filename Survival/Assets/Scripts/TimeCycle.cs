d8a461a99e66f1525d56fde095b1792f5fb1192c
Collcroc123 <Collin.g086@gmail.com>
2023-06-01 18:04:28 -0600

    Sky Overhaul

--- Survival/Assets/Scripts/TimeCycle.cs
+++ Survival/Assets/Scripts/TimeCycle.cs
@@ -4,7 +4,7 @@ using System;
 
 public class TimeCycle : MonoBehaviour
 {
-    private Light sun;
+    [SerializeField] private Light sun;
     [SerializeField, Range(0,24)] private float time;
     public Gradient lightColor;
     public Gradient ambientColor;
@@ -13,20 +13,23 @@ public class TimeCycle : MonoBehaviour
     [SerializeField] TextMeshProUGUI timeText;
     public bool twelveHourFormat;
 
-    void Start() => sun = GetComponent<Light>();
+    //void Start() => sun = GetComponent<Light>();
 
-    void Update()
+    private void OnValidate() => SetTime();
+
+    void Update() => SetTime();
+
+    private void SetTime()
     {
-        if(Application.isPlaying)
-        {
-            time += Time.deltaTime/60;
-            time %= 24;
-            UpdateTime((time / 24f));
+        Shader.SetGlobalVector("_SunDirection", transform.forward);
+        Shader.SetGlobalVector("_MoonDirection", -transform.forward);
+        time += Time.deltaTime / 60;
+        time %= 24;
+        UpdateTime((time / 24f));
 
-            DateTime dateTime = DateTime.Today.Add(TimeSpan.FromHours(time));
-            if (twelveHourFormat) timeText.text = dateTime.ToString("hh:mm tt");
-            else timeText.text = dateTime.ToString("HH:mm");
-        }
+        DateTime dateTime = DateTime.Today.Add(TimeSpan.FromHours(time));
+        if (twelveHourFormat) timeText.text = dateTime.ToString("hh:mm tt");
+        else timeText.text = dateTime.ToString("HH:mm");
     }
 
     private void UpdateTime(float percent)
